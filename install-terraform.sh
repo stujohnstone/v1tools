6c4813044c5abe3ac4fd5cb91dd8a69b729ed164
Stu <stu.johnstone@vistair.com>
2020-05-22 15:47:06 +0100

    added new service

--- install-terraform.sh
+++ install-terraform.sh
@@ -3,3 +3,4 @@ wget https://releases.hashicorp.com/terraform/0.12.25/terraform_0.12.25_linux_am
 unzip terraform_0.12.25_linux_amd64.zip
 sudo mv terraform /usr/local/bin/
 terraform --version 
+rm ./terraform_0.12.25_linux_amd64.zip
